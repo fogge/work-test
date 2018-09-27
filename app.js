eab37069bc089fd4a801a768ef513489c8eaacb4
Unknown <andreas@stickydirt.se>
2018-09-27 12:49:48 +0200

    Add sass, loader, functions for startpage.

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 // Require the express module
 const express = require('express');
+const path = require('path');
+require('all-that-sass')({
+  watch: path.join(__dirname, './www/scss'),
+  input: path.join(__dirname, './www/scss/style.scss'),
+  output: path.join(__dirname, './www/style.css'),
+  reportErrors: true,
+  reportCompiles: false,
+  outputStyle: 'expanded'
+});
+
 // Create a new web server
 const app = express();
 // Tell the web server to serve files
 // from the www folder
 app.use(express.static('www'));
+
+
+
+
 // Start the web server on port 3000
 app.listen(3000,() => console.log('Listening on port 3000'));
--- www/app.js
+++ www/app.js
@@ -1,37 +1,52 @@
-let mynewdata;
-
-let xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=ANnB9Knl5Ao5IfSVzu1bUTeymhbRTYLY&limit=20")
-.done((data) => { 
-  mynewdata = data;
-})
-.always(() => {
-  $('.loader').hide();
-}).then(startEverything);
-
-function startEverything(){
-  let renderGifs = getRenderingItems(mynewdata.data)
-  $('.container').append(renderGifs);
+function app(){
+  loadGifs();
   $(window).scroll(() => {
-    console.log($(window).scrollTop(), $(document).height(),  $(window).height())
-    if($(window).scrollTop() >= $(document).height() - $(window).height() -2) {
-      console.log('i should render more now...')
-          // ajax call get data from server and append to the div
-          $('.loader').show();
-          xhr = $.get("http://api.giphy.com/v1/gifs/search?q=cats&api_key=ANnB9Knl5Ao5IfSVzu1bUTeymhbRTYLY&limit=20")
-          .done((data) => { 
-          renderGifs = getRenderingItems(data.data);
-          $('.container').append(renderGifs);
-          }).always(() => $('.loader').hide());
+    if($(window).scrollTop() >= $(document).height() - $(window).height() -200 && $('.loader').hasClass('d-none')) {
+      loadGifs();
     }
   });
 }
 
+function loadGifs(){
+  let offset = $('img').length;
+  $('.loader').removeClass('d-none');
+  $.get(`http://api.giphy.com/v1/gifs/trending?&api_key=ANnB9Knl5Ao5IfSVzu1bUTeymhbRTYLY&limit=20&offset=${offset}`)
+  .done((data) => { 
+    $('.container article').append(getRenderingItems(data.data));
+    checkIfAllLoaded();
+  });
+};
+
+
+function checkIfAllLoaded() {
+  let imgs = $('img'),
+  imgLength = imgs.length,
+  counter = imgLength - 20;
+
+  imgs.each((i, x) => {
+    console.log('this is in each statement!', x);
+    x.addEventListener( 'load', increaseAndCheck, false );
+
+  });
+  function increaseAndCheck(){
+    counter++;
+    console.log(counter, imgLength);
+    if (counter == imgLength) {
+      console.log('hellooo');
+      $('.loader').addClass('d-none');
+    }
+  }
+}
+
+
 function getRenderingItems(arr){
   return arr.map(gifObj => {
     return `
-    <div class="gif-holder">
-      <img src="${gifObj.images.original.url}" alt="Another gif bites the dust" class="gif">
+    <div class="gif-holder col-6 d-flex align-items-center justify-content-center">
+      <img src="${gifObj.images.original.url}" alt="Another gif bites the dust" class="col-12">
     </div>
     `
   })
 }
+
+app();
